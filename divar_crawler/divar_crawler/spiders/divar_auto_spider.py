81bf3fe77db560a9f5909f54ed6d0d62c0b945e4
Alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-10-12 12:39:23 +0330

    continue with endpoint and there is same problem yet.

--- divar_crawler/divar_crawler/spiders/divar_auto_spider.py
+++ divar_crawler/divar_crawler/spiders/divar_auto_spider.py
@@ -1,32 +1,3 @@
-# import time
-#
-# import scrapy
-#
-# class DivarSpider(scrapy.Spider):
-#     name = 'divar_auto'
-#     allowed_domains = ['divar.ir']
-#     start_urls = ['https://divar.ir/s/shiraz/auto']
-#     download_delay = 1
-#     page_number = 1
-#
-#     def parse(self, response):
-#         items = response.xpath('//div[@class="post-list__widget-col-c1444"]')
-#         for item in items:
-#             time.sleep(2)
-#             title = item.xpath('.//h2/text()').get()
-#             price = item.xpath('.//div[@class="kt-post-card__description"][2]/text()').get()
-#             yield {
-#                 'title': title,
-#                 'price': price if price else None,
-#             }
-#         self.page_number += 1
-#         next_page = f'https://divar.ir/s/shiraz/auto?page={self.page_number}'
-#         yield scrapy.Request(next_page, callback=self.parse)
-
-
-
-
-
 import scrapy
 import json
 
@@ -35,22 +6,21 @@ class DivarCarSpider(scrapy.Spider):
     name = "divar_auto"
     allowed_domains = ["api.divar.ir"]
     start_urls = ['https://api.divar.ir/v8/postlist/w/search']
-
-
     brands = ['arisun', 'ario', 'alfa-romeo', 'mvm', 'amico', 'opel', 'swm', 'mg',
-              'iran-khodro', 'baic', 'brilliance', 'mercedes-benz', 'saipa']
-
+              'iran-khodro', 'baic', 'brilliance', 'mercedes-benz', 'saipa', 'besturn',
+              'bmw', 'pars-khodro', 'peugeot', 'porsche', 'tara', 'renault', 'toyota',
+              'tiba', 'tigard', 'jac', 'jeep', 'dena', 'geely', 'dongfeng', 'chery',
+              'audi', 'respect', 'runna', 'ssangyong', 'saina', 'samand', 'ford',
+              'fownix']
 
     brand_index = 0
 
     def start_requests(self):
-
         if self.brand_index < len(self.brands):
             brand = self.brands[self.brand_index]
             yield self.make_request_for_brand(brand, 1)
 
     def make_request_for_brand(self, brand, page):
-
         payload = {
             "city_ids": ["6"],
             "search_data": {
@@ -68,6 +38,7 @@ class DivarCarSpider(scrapy.Spider):
             "sort": {"str": {"value": "sort_date"}},
             "page": page
         }
+
         return scrapy.Request(
             url='https://api.divar.ir/v8/postlist/w/search',
             method="POST",
@@ -82,7 +53,6 @@ class DivarCarSpider(scrapy.Spider):
         brand = response.meta['brand']
         page = response.meta['page']
 
-
         for item in data.get('list_widgets', []):
             if item.get('widget_type') == 'POST_ROW':
                 car_data = item['data']
@@ -95,13 +65,10 @@ class DivarCarSpider(scrapy.Spider):
                     'price': price,
                 }
 
-
         if data.get('pagination', {}).get('has_next_page'):
             next_page = page + 1
-
             yield self.make_request_for_brand(brand, next_page)
         else:
-
             self.brand_index += 1
             if self.brand_index < len(self.brands):
                 next_brand = self.brands[self.brand_index]
