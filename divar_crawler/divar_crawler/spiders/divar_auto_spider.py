4af151a0d896f597ef087bccf68c060481285b5e
Alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-10-12 11:25:25 +0330

    first response correctly work but doesn't loop for all brand.

--- divar_crawler/divar_crawler/spiders/divar_auto_spider.py
+++ divar_crawler/divar_crawler/spiders/divar_auto_spider.py
@@ -25,102 +25,6 @@
 
 
 
-# import scrapy
-# import json
-# class DivarCarSpider(scrapy.Spider):
-#     name = 'divar_auto'
-#     allowed_domains = ['divar.ir']
-#
-#     brands = ['arisun', 'ario', 'alfa-romeo', 'mvm', 'amico', 'opel', 'swm', 'mg', 'iran-khodro', 'baic', 'brilliance',
-#               'mercedes-benz']
-#     city_id = "6"
-#     brand_index = 0
-#
-#     def start_requests(self):
-#
-#         brand = self.brands[self.brand_index]
-#         url = 'https://api.divar.ir/v8/postlist/w/search'
-#
-#         form_data = {
-#             "data": {
-#                 "brand_model": {"repeated_string": {"value": [brand]}},
-#                 "category": {"str": {"value": "light"}}
-#             }
-#         }
-#
-#         server_payload = {
-#             "@type": "type.googleapis.com/widgets.SearchData.ServerPayload",
-#             "additional_form_data": {
-#                 "data": {"sort": {"str": {"value": "sort_date"}}}
-#             }
-#         }
-#
-#         cities = [self.city_id]
-#
-#
-#         headers = {
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
-#             'Content-Type': 'application/json',
-#         }
-#
-#         payload = {
-#             'form_data_json': json.dumps(form_data),
-#             'server_payload_json': json.dumps(server_payload),
-#             'cities': cities
-#         }
-#
-#         yield scrapy.Request(
-#             url=url,
-#             method="POST",
-#             headers=headers,
-#             body=json.dumps(payload),
-#             callback=self.parse,
-#             meta={'brand': brand}
-#         )
-#
-#     def parse(self, response):
-#         data = json.loads(response.text)
-#         brand = response.meta['brand']
-#
-#
-#         for widget in data.get('list_widgets', []):
-#             if widget.get('widget_type') == 'POST_ROW':
-#                 car_data = widget.get('data', {})
-#                 title = car_data.get('title', 'N/A')
-#                 price = car_data.get('middle_description_text', 'N/A')
-#                 mileage = car_data.get('top_description_text', 'N/A')
-#                 city = car_data.get('city', 'shiraz')
-#                 image_url = car_data.get('image_url', 'N/A')
-#
-#
-#                 yield {
-#                     'brand': brand,
-#                     'title': title,
-#                     'price': price,
-#                     'mileage': mileage,
-#                     'city': city,
-#                     'image_url': image_url,
-#                 }
-#
-#
-#         if 'last_post_date' in data:
-#             last_post_date = data['last_post_date']
-#             url = f'https://api.divar.ir/v8/postlist/w/search?last-post-date={last_post_date}'
-#
-#
-#             yield scrapy.Request(
-#                 url=url,
-#                 method="POST",
-#                 headers=response.request.headers,
-#                 body=response.request.body,
-#                 callback=self.parse,
-#                 meta={'brand': brand}
-#             )
-#         else:
-#
-#             self.brand_index += 1
-#             if self.brand_index < len(self.brands):
-#                 yield from self.start_requests()
 
 
 import scrapy
@@ -130,18 +34,30 @@ import json
 class DivarCarSpider(scrapy.Spider):
     name = "divar_auto"
     allowed_domains = ["api.divar.ir"]
-    start_urls = [
-        'https://api.divar.ir/v8/postlist/w/search'
-    ]
+    start_urls = ['https://api.divar.ir/v8/postlist/w/search']
+
+
+    brands = ['arisun', 'ario', 'alfa-romeo', 'mvm', 'amico', 'opel', 'swm', 'mg',
+              'iran-khodro', 'baic', 'brilliance', 'mercedes-benz', 'saipa']
+
+
+    brand_index = 0
 
     def start_requests(self):
+
+        if self.brand_index < len(self.brands):
+            brand = self.brands[self.brand_index]
+            yield self.make_request_for_brand(brand, 1)
+
+    def make_request_for_brand(self, brand, page):
+
         payload = {
             "city_ids": ["6"],
             "search_data": {
                 "form_data": {
                     "data": {
                         "brand_model": {
-                            "repeated_string": {"value": ["Arisan"]}
+                            "repeated_string": {"value": [brand]}
                         },
                         "category": {
                             "str": {"value": "light"}
@@ -149,40 +65,44 @@ class DivarCarSpider(scrapy.Spider):
                     }
                 }
             },
-            "sort": {"str": {"value": "sort_date"}}
+            "sort": {"str": {"value": "sort_date"}},
+            "page": page
         }
-
-        yield scrapy.Request(
+        return scrapy.Request(
             url='https://api.divar.ir/v8/postlist/w/search',
             method="POST",
             body=json.dumps(payload),
             headers={'Content-Type': 'application/json'},
-            callback=self.parse
+            callback=self.parse,
+            meta={'brand': brand, 'page': page}
         )
 
     def parse(self, response):
         data = response.json()
+        brand = response.meta['brand']
+        page = response.meta['page']
+
 
         for item in data.get('list_widgets', []):
             if item.get('widget_type') == 'POST_ROW':
                 car_data = item['data']
                 title = car_data.get('title')
-                price = car_data.get('bottom_description_text', 'N/A')
+                price = car_data.get('middle_description_text', 'N/A')
 
                 yield {
+                    'brand': brand,
                     'title': title,
                     'price': price,
                 }
 
+
         if data.get('pagination', {}).get('has_next_page'):
-            next_page = data['pagination']['page'] + 1
-            payload['page'] = next_page
-            yield scrapy.Request(
-                url='https://api.divar.ir/v8/postlist/w/search',
-                method="POST",
-                body=json.dumps(payload),
-                headers={'Content-Type': 'application/json'},
-                callback=self.parse
-            )
+            next_page = page + 1
 
+            yield self.make_request_for_brand(brand, next_page)
+        else:
 
+            self.brand_index += 1
+            if self.brand_index < len(self.brands):
+                next_brand = self.brands[self.brand_index]
+                yield self.make_request_for_brand(next_brand, 1)
