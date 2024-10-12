9128caabe031e598639f449d73bbc82bb87890c0
Alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-10-12 18:14:13 +0330

    I try so much but the problem exists yet.

--- divar_crawler/divar_crawler/spiders/divar_auto_spider.py
+++ divar_crawler/divar_crawler/spiders/divar_auto_spider.py
@@ -1,7 +1,5 @@
 import scrapy
 import json
-
-
 class DivarCarSpider(scrapy.Spider):
     name = "divar_auto"
     allowed_domains = ["api.divar.ir"]
@@ -16,41 +14,40 @@ class DivarCarSpider(scrapy.Spider):
     brand_index = 0
 
     def start_requests(self):
-        if self.brand_index < len(self.brands):
             brand = self.brands[self.brand_index]
             yield self.make_request_for_brand(brand, 1)
 
     def make_request_for_brand(self, brand, page):
-        payload = {
-            "city_ids": ["6"],
-            "search_data": {
-                "form_data": {
-                    "data": {
-                        "brand_model": {
-                            "repeated_string": {"value": [brand]}
-                        },
-                        "category": {
-                            "str": {"value": "light"}
+                payload = {
+                    "city_ids": ["6"],
+                    "search_data": {
+                        "form_data": {
+                            "data": {
+                                "brand_model": {
+                                    "repeated_string": {
+                                        "value": [brand]}
+                                },
+                                "category": {
+                                    "str": {"value": "light"}
+                                }
+                            }
                         }
-                    }
+                    },
+                    "sort": {"str": {"value": "sort_date"}},
+                    "page": page
                 }
-            },
-            "sort": {"str": {"value": "sort_date"}},
-            "page": page
-        }
-
-        return scrapy.Request(
-            url='https://api.divar.ir/v8/postlist/w/search',
-            method="POST",
-            body=json.dumps(payload),
-            headers={'Content-Type': 'application/json'},
-            callback=self.parse,
-            meta={'brand': brand, 'page': page}
-        )
+                return scrapy.Request(
+                    url='https://api.divar.ir/v8/postlist/w/search',
+                    method="POST",
+                    body=json.dumps(payload),
+                    headers={'Content-Type': 'application/json'},
+                    callback=self.parse,
+                    meta={'value': brand, 'page': page}
+                )
 
     def parse(self, response):
         data = response.json()
-        brand = response.meta['brand']
+        brand = response.meta['value']
         page = response.meta['page']
 
         for item in data.get('list_widgets', []):
@@ -58,18 +55,22 @@ class DivarCarSpider(scrapy.Spider):
                 car_data = item['data']
                 title = car_data.get('title')
                 price = car_data.get('middle_description_text', 'N/A')
-
+                milage = car_data.get('top_description_text', 'N/A')
                 yield {
                     'brand': brand,
                     'title': title,
                     'price': price,
-                }
+                    'milage': milage,
+                    }
 
         if data.get('pagination', {}).get('has_next_page'):
             next_page = page + 1
             yield self.make_request_for_brand(brand, next_page)
-        else:
-            self.brand_index += 1
-            if self.brand_index < len(self.brands):
-                next_brand = self.brands[self.brand_index]
-                yield self.make_request_for_brand(next_brand, 1)
+        self.brand_index += 1
+        if self.brand_index < len(self.brands):
+            next_brand = self.brands[self.brand_index]
+            yield self.make_request_for_brand(next_brand, 1)
+
+
+
+
